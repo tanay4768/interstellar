87a40cc061c17345e5e6751052c1e8d9cd44a849
tanay4768 <tanay872.exwork@gmail.com>
2025-07-31 19:25:02 +0530

    Day 2 and Day 3

--- src/main.js
+++ src/main.js
@@ -90,16 +90,68 @@ function main() {
 
 	}
 
+	class DegRadHelper {
+
+		constructor( obj, prop ) {
+
+			this.obj = obj;
+			this.prop = prop;
+
+		}
+		get value() {
+
+			return THREE.MathUtils.radToDeg( this.obj[ this.prop ] );
+
+		}
+		set value( v ) {
+
+			this.obj[ this.prop ] = THREE.MathUtils.degToRad( v );
+
+		}
+
+	}
+
+	function makeXYZGUI( gui, vector3, name, onChangeFn ) {
+
+		const folder = gui.addFolder( name );
+		folder.add( vector3, 'x', - 10, 10 ).onChange( onChangeFn );
+		folder.add( vector3, 'y', 0, 10 ).onChange( onChangeFn );
+		folder.add( vector3, 'z', - 10, 10 ).onChange( onChangeFn );
+		folder.open();
+
+	}
+
 	{
 
 		const color = 0xFFFFFF;
-		const intensity = 1;
-		const light = new THREE.AmbientLight( color, intensity );
+		const intensity = 150;
+		const light = new THREE.SpotLight( color, intensity );
+		light.position.set( 0, 10, 0 );
+		light.target.position.set( - 5, 0, 0 );
 		scene.add( light );
+		scene.add( light.target );
+
+		const helper = new THREE.SpotLightHelper( light );
+		scene.add( helper );
+
+		function updateLight() {
+
+			light.target.updateMatrixWorld();
+			helper.update();
+
+		}
+
+		updateLight();
 
 		const gui = new GUI();
 		gui.addColor( new ColorGUIHelper( light, 'color' ), 'value' ).name( 'color' );
-		gui.add( light, 'intensity', 0, 5, 0.01 );
+		gui.add( light, 'intensity', 0, 250, 1 );
+		gui.add( light, 'distance', 0, 40 ).onChange( updateLight );
+		gui.add( new DegRadHelper( light, 'angle' ), 'value', 0, 90 ).name( 'angle' ).onChange( updateLight );
+		gui.add( light, 'penumbra', 0, 1, 0.01 );
+
+		makeXYZGUI( gui, light.position, 'position', updateLight );
+		makeXYZGUI( gui, light.target.position, 'target', updateLight );
 
 	}
 
